f35aa775abda5d37792f9645ec59b7a49506a4e8
Hershal Rami <hershalrami@gmail.com>
2020-06-26 23:23:28 -0400

    Rewrote Schedule's add_section method to check that the section does not overlap with the existing courses. Relies on the boolean array/heat map idea, one variable needs to be renamed before compiling

--- Schedule.py
+++ Schedule.py
@@ -9,6 +9,7 @@ class Schedule:
 
     def __init__(self):
         self.sections = []      # Array of Section objects
+        self.compiled_sections = [] # 2D array acting as a boolean heat-map for course times
 
     def __str__(self):
         out = "{"
@@ -19,8 +20,42 @@ class Schedule:
 
         return out
 
+    # Compares the boolean array of the new section to the rest of schedule
+    # Adds the section and returns true if legal, returns false otherwise
     def add_section(self, section):
+        #TODO section.boolean_array is a placeholder for the section data
+
+        # Stores ordered tuples representing indices for active time slots
+        active_slots = []
+
+        # Finds all active slots in the section parameter and stores them in active_slots
+        for x in range(5):
+            # represents each day of the week M-F
+            day = section.boolean_array[x]
+
+            # 6am-11pm = 17 hours = 68 time slots of 15 mins each
+            for y in range(68):
+                # Represents a single 15 min slot
+                slot = day[y]
+                if slot == True:
+                    active_slots.append([x,y])
+        
+        # Compare active_slots to compiled master list
+        for indices in active_slots:
+            day = indices[0][0]
+            slot = indices[0][1]
+
+            if self.compiled_sections[day][slot] == True:
+                # The new section overlaps with an existing section
+                return False
+        
+        # New section does not overlap, add it to the schedule and return true
         self.sections.append(Section.Section(section))
+        for indices in active_slots:
+            day = indices[0][0]
+            slot = indices[0][1]
+            self.compiled_sections[day][slot] = True
+        return True
     
     def add_sections(self, sections):
         for section in sections:
