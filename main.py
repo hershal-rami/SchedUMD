f5560edf796c06f2b5c87bc1c8d59faef0505633
Hershal Rami <hershalrami@gmail.com>
2020-07-17 01:41:56 -0400

    Wrote a majority of the generate_possibilities method in main. Still need to fix edge cases/properly increment counter_array for the method to work as intended. Next steps are to fix those bugs, test the code out, then begin working on front end. We can come back to this later and introduce hashsets, threading, and search filters in the future

--- main.py
+++ main.py
@@ -13,11 +13,67 @@ import requests
 import Course
 import CourseList
 
+# Traverses the given CourseList with a modified brute force algorithm
+# Returns list containing all possible Schedule objects
 def generate_possibilities(course_list):
-    #Brute force schedules, return some data structure containing of them
-    schedule_list = []  # List containing all possible schedules
+    courses = course_list.get_courses() # Array of Course objects
+    num_courses = courses.len() # Number of Courses being taken
 
-    pass
+    all_sections = [] # 2D array holding list of all sections for each course
+    schedule_list = [] # Array holding all possible Schedules
+
+    # populate the array with section data from provided CourseList
+    for course in courses:
+        all_sections.append(course.sections)
+
+    # holds counters for each course's array, initialize all to 0
+    counter_array = [0] * num_courses
+
+    while True:
+        # Create a new Schedule
+        schedule = Schedule.Schedule()
+        
+        # Traverses all_sections to make a Schedule
+        for x in range(num_courses):
+            # TODO this breaks if the last section of a course has a conflict
+            # Loop to ensure that a section is added from this course
+            while True:
+                # Pulls the next section for the course and attempts to add it
+                section = all_sections[x][counter_array[x]]
+                result = schedule.add_section(section)
+
+                # Only loop if there was a conflict
+                if not result:
+                    # Increment counter for this Course to avoid future conflicts
+                    counter_array[x] += 1
+                else:
+                    break
+
+        # Add the new Schedule to the master list
+        schedule_list.append(schedule)
+
+        # TODO ok ive realized like, all of this increment shit fails several edge cases and my attempts to
+        # rework it are not really panning out rn, ill fix it later lol
+        # Increment counter_array as needed
+        reset = False
+        for x in reversed(range(num_courses)):
+            counter = counter_array[x]
+            num_sections = courses[x].sections.len()
+
+            # No more sections for this course
+            if (counter + 1) == num_sections:
+                if not reset:
+                    # Only ever want to increment 1 counter, even though sometimes you may need to reset more than 1
+                    # Reset counter and increment previous counter to move to a different branch
+                    counter_array[x] = 0
+                    counter_array[x - 1] += 1
+                    reset = True
+            else:
+                if not reset:
+                    counter_array[x] += 1
+                    reset = True
+                break
+
 
 benSc = CourseList.CourseList()
 hershSc = CourseList.CourseList()
